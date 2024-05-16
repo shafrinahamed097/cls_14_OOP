fb5c32b46f1fb0a905ff15be057a61278cb7cd80
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-16 11:38:12 +0600

    Declare Parent keyword

--- inheritance.php
+++ inheritance.php
@@ -2,6 +2,7 @@
 
 // Inheritance
 // Overriding
+// Parent Keyword 
 
 class Father{
     public function print100(){ 
@@ -14,15 +15,36 @@ class Father{
 
 class Son extends Father{
 
-    public function print100(){ 
-
-        for($i=0; $i<=100; $i=$i+10){
-            echo "$i" ."\n" ;
-    }
-}
-
+    
 
 }
 
 $SonObj = new Son();
 $SonObj->print100();
+
+// Overriding
+
+class Son1 extends Father{
+    public function print100(){ 
+        for($i=0; $i<=100; $i+=10){
+            echo "$i" ."\n" ;
+        }
+    }
+}
+
+$SonObj1 = new Son1();
+$SonObj1->print100();
+
+// Parents Keyword = The parent keyword refers to the parent class of the current class.
+// It can be used to access properties and methods of the parent class.
+
+class Son2 extends Father{
+    public function demo(){
+        parent::print100();
+
+    }
+}
+
+$SonObj2 = new Son2();
+$SonObj2->demo();
+
